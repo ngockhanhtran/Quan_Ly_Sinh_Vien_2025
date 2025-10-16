f5ff7eadad94bc4d86ef838bd0bfff33c80269df
Tran Ngoc Khanh <trankhanh01092006@gmail.com>
2025-10-16 21:14:12 +0700

    Thu Vien

--- Thu Vien/AccountManager.h
+++ Thu Vien/AccountManager.h
@@ -14,7 +14,7 @@ public:
     Account* login(const string& username, const string& password);
     bool registerStudent(const string& studentID); // mặc định password
     bool changePassword(Account& account, const string& oldPass, const string& newPass);
-    bool resetPassword(const string& studentID, const string& newPass = "12345@"); // only admin use
+    bool resetPassword(const string& studentID, const string& newPass = "12345@"); // only admin can use
 
     void showMenu();
 };
