160ca0e37d459a7f44ff6126aebe19310a8844ce
Tran Ngoc Khanh <trankhanh01092006@gmail.com>
2025-10-16 21:22:16 +0700

    Thuc Thi

--- Thuc Thi/AccountManager.cpp
+++ Thuc Thi/AccountManager.cpp
@@ -35,4 +35,62 @@ void AccountManager ::saveToFile(const string& filename){
     if(!file.is_open()){    
         cerr << "Khong the ghi file" << filename << endl;
     }
+
+    file << "username, password, type, linkedpersonID\n";
+    for(const auto& acc: accounts){
+        file << acc.getUsername() << "," << acc.getPassword() << "," 
+            << Account ::AccounttoString(acc.getType()) << ","
+            <<acc.getID() << "\n";
+    }
+    file.close();
+}
+
+Account* AccountManager ::login(const string& username, const string& password){
+    for(auto& acc:accounts){
+        if(acc.authenticate(username, password)){
+            return &acc;
+        }
+    }
+    return nullptr;
+}
+
+bool AccountManager ::registerStudent(const string& studentID){
+    for(const auto& acc : accounts){
+        if(acc.getUsername() == studentID){
+            return false;
+        }
+        accounts.emplace_back(studentID, "12345@", AccountType::STUDENT, studentID);
+        return true;
+    }
+}
+
+bool AccountManager ::changePassword(Account& account, const string& oldPass, const string& newPass){
+    return account.changePass(oldPass, newPass);
+}
+// reset password
+bool AccountManager ::resetPassword(const string& studentID, const string& newPass = "12345@"){
+    for (auto& acc : accounts){
+        if (acc.getUsername() == studentID && acc.getType() == AccountType::STUDENT){
+            acc.setPassword(newPass);
+            return true;
+        }
+    }
+    return false;
+}
+
+void AccountManager ::showMenu(){
+    const string filename = "accounts.csv";
+    loadFromFile(filename);
+
+    while(true){
+        cout << "\n====== MENU TAI KHOAN ======\n";
+        cout << "1. Dang Nhap\n";
+        cout << "2. Dang ky\n";
+        cout << "3. Thoat\n";
+        cout << "============================\n";
+        cout << "Chon: ";
+        int choice; cin >> choice;
+        cin.ignore();
+        
+    }
 }
